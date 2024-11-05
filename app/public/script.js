0902e4a64eae1b94ec90fc4ef9bd555ecdf71157
AbdulConsole <hzwedt@gmail.com>
2024-11-05 07:16:35 +0100

    new ⛈️ app

---
+++ app/public/script.js
@@ -0,0 +1,9 @@
+document.getElementById('getStarted').addEventListener('click', function() {
+    // Action when 'Get Started' button is clicked
+    window.location.href = 'student.html'; // Redirect to student page
+});
+
+document.getElementById('instructorLogin').addEventListener('click', function() {
+    // Action when 'Instructor Login' button is clicked
+    window.location.href = 'instructor.html'; // Redirect to instructor login page
+});
